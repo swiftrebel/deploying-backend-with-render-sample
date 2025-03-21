2dbf97fdcfbdadf38e55344863dd00fc64078de5
swiftrebel <88529956+swiftrebel@users.noreply.github.com>
2025-03-21 09:00:01 -0500

    fixing error in meow api

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ async function getRandomActivity() {
     const response = await fetch(BORED_API_BASE_URL);
     if (response.ok) {
       const data = await response.json();
-      return data;
+      return data.data[0];
     } else {
       return null;
     }
